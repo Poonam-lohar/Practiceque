5209ed7876aba187dc692b2e070600910451653a
Your Name <you@example.com>
2022-08-16 10:36:08 +0530

    added:welcome to Practice Quetion

---
+++ src/pracq1.java
@@ -0,0 +1,11 @@
+public class pracq1 {
+    public static void main(String[] args) {
+        int num = -2;
+        if (num > 0) {
+            System.out.println("munber is positive");
+        } else if (num < 0) {
+            System.out.println("number is negative");
+        } else
+            System.out.println("invalid");
+    }
+}
